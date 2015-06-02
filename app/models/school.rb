535b903a88950ef7ee7564402457ec889fc0976c
Aaron Harvey <harvnlenny@gmail.com>
2015-06-01 21:38:37 -0400

    home page is working

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,3 @@
 class School < ActiveRecord::Base
-  validates :ending_year, numericality: {greater_than_or_equal_to: beginning_year}
+  #validates :ending_year, numericality: {greater_than_or_equal_to: beginning_year }
 end

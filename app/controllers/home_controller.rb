3bb5d6e048901d127d72d520b254bd3e5599c8c0
Aaron Harvey <harvnlenny@gmail.com>
2015-06-03 20:20:50 -0400

    got last message to post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-   
+    @schools = School.all
+    @school = @schools.last
+    @life_events = LifeEvent.all
+    @life_event = @life_events.last
+
   end
 end
